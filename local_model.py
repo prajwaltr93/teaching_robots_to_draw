2d49cda71dc5c0785196de05328b815841c39652
Prajwal T R <prajwaltr93@gmail.com>
2020-10-30 08:51:08 +0530

    fully trained local model and its weights added

--- local_model.py
+++ local_model.py
@@ -1,138 +1,108 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[14]:
-
-
-import pickle as pic
-#data preparation :: training on single dataset
-path = '/home/starkm42/hershey_datset/local_dataset/'
-
-fd = open(path + 'data_batch_0', 'rb')
-data = pic.load(fd, encoding="bytes")
-
-sample_rate_train = 100
-lg_data = data['lG_data'][:sample_rate_train]
-lg_extract = data['lG_extract'][:sample_rate_train]
-lg_touch = data['lG_touch'][:sample_rate_train]
-lg_croppedimg = data['lG_croppedimg'][:sample_rate_train]
-
-# len(lg_data)
-
-
-# In[2]:
-
-
-#imports
-import numpy as np
-
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import Conv2D
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import MaxPooling2D
 from tensorflow.keras.layers import add
-from tensorflow.keras.utils import plot_model
+from tensorflow.keras.layers import Dropout
 from tensorflow.keras.layers import BatchNormalization
-from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.utils import Sequence
 
+# force graph building by disabling eager execution
 import tensorflow as tf
 tf.compat.v1.disable_eager_execution()
 tf.compat.v1.experimental.output_all_intermediates(True)
 
+weights_path = "./weights/local_model_weights/local_model_trained_1"
 
-# In[3]:
-
-
-#residual module
-def res_module(inp):
-    if not inp.shape[-1] == 64:
-        #project with 1x1 convolution
-        inp = Conv2D(64,1,activation="relu")(inp)
+# build , intiliaze with trained weights and return model for inference
+def getLocalModel():
+    inp = Input(shape=(100, 100, 16))
     x = BatchNormalization()(inp)
     x = Activation("relu")(x)
-    x = Conv2D(16, 3,padding="same",activation="sigmoid")(x)
-    x = Conv2D(32, 3,padding="same",activation="sigmoid")(x)
-    x = Conv2D(64, 3,padding="same",activation="sigmoid")(x)
+    x = Conv2D(16, 3,padding="same")(x)
     x = BatchNormalization()(x)
     x = Activation("relu")(x)
-    x = Conv2D(16, 3,padding="same",activation="sigmoid")(x)
-    x = Conv2D(32, 3,padding="same",activation="sigmoid")(x)
-    x = Conv2D(64, 3,padding="same",activation="sigmoid")(x)
+    x = Conv2D(16, 3,padding="same")(x)
     out = add([x, inp])
-    return out
+    res_block_16 = Model(inputs=inp, outputs=out)
 
+    inp = Input(shape=(100, 100, 16))
+    x = BatchNormalization()(inp)
+    x = Activation("relu")(x)
+    x = Conv2D(16, 3,padding="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    x = Conv2D(16, 3,padding="same")(x)
+    out = add([x, inp])
+    res_block_16_1 = Model(inputs=inp, outputs=out)
 
-# In[10]:
+    #residual module
+    inp = Input(shape=(100, 100, 16))
+    x = BatchNormalization()(inp)
+    x = Activation("relu")(x)
+    x = Conv2D(16, 3,padding="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    x = Conv2D(32, 3,padding="same")(x)
+    if not inp.shape[-1] == 32:
+      #project with 1x1 convolution
+      con = Conv2D(32,1)(inp)
+    out = add([x, con])
+    res_block_32 = Model(inputs=inp, outputs=out)
 
+    #residual module
+    inp = Input(shape=(100, 100, 32))
+    x = BatchNormalization()(inp)
+    x = Activation("relu")(x)
+    x = Conv2D(16, 3,padding="same")(x)
+    x = BatchNormalization()(x)
+    x = Activation("relu")(x)
+    x = Conv2D(64, 3,padding="same")(x)
+    if not inp.shape[-1] == 64:
+      #project with 1x1 convolution
+      con = Conv2D(64,1)(inp)
+    out = add([x, con])
+    res_block_64 = Model(inputs=inp, outputs=out)
 
-#creating local model
+    """Building Model
 
-lG_inp = Input(shape = (95, 65, 3), name = "lg_inp")
+    4 Block Residual block with dynamic tensor extraction
 
-ext_inp = Input(shape = (3), dtype = tf.int32, name = "ext_inp")
+    two input, two output model (keras functional API)
+    """
 
-#defining model
+    # tensor slice
+    def slice_return(x):
+      return tf.slice(x[0], x[1], [5,5,64])
 
-#four residual module stacked 
-x = res_module(lG_inp)
-x = res_module(x)
-x = res_module(x)
-x = res_module(x)
+    #creating local model
+    inp = Input(shape = inp_img_dim, dtype=tf.float32, name = "lg_inp")
+    ext_inp = Input(shape = inp_ext_dim, dtype = tf.int32, name = "ext_inp")
 
-# now x is 95 * 65 * 54 res encoded tensor, now carry out extraction procedure to enforce localization
-# batch_size is Dynamic, Unknown or of type None, hence using map_fn to iterate over dimention '0' --> None
+    # initilization layer, helpful in transfer learning, due to different input layers, between global and local models
+    conv = Conv2D(16, 3, padding='same')(inp)
+    #four residual block stacked
+    x_a = res_block_16(conv)
+    x_a = res_block_16_1(x_a)
+    x_a = res_block_32(x_a)
+    x_a = res_block_64(x_a)
 
-extracted_tensor = tf.map_fn(lambda x : tf.slice(x[0], x[1], [5,5,64]), elems = (x, ext_inp), dtype = (tf.float32))
+    # now x is 95 * 65 * 54 res encoded tensor, now carry out extraction procedure to enforce localization
+    # batch_size is Dynamic, Unknown or of type None, hence using map_fn to iterate over dimention '0' --> None
+    extracted_tensor = tf.map_fn(slice_return, elems = (x_a, ext_inp), fn_output_signature=tf.float32)
+    x_0 = Flatten()(extracted_tensor) #flatten and feed to dense layer
+    #fully connected layer 1
+    # x1 = Dense(256, activation='relu')(x_0)
+    x1 = Dense(128, activation='relu')(x_0)
+    #fully connected layer 2
+    x2 = Dense(1, activation='sigmoid', name = 'out_touch')(x1)
+    #fully connected layer 3
+    x3 = Dense(25, activation='softmax', name = 'out_cropped')(x1)
+    # x3 = tf.reshape(x3, (5,5)) #output a 5 * 5 image
+    model = Model(inputs= [inp, ext_inp], outputs= [x2,x3])
 
-x = Flatten()(extracted_tensor) #flatten and feed to dense layer
+    # initialize with trained weights
+    model.load_weights(weights_path)
 
-#fully connected layer 1
-
-x1 = Dense(128, activation='relu')(x)
-
-#fully connected layer 2
-x2 = Dense(1, activation='softmax', name = 'out_touch')(x1)
-
-#fully connected layer 3
-x3 = Dense(25, activation='softmax', name = 'out_cropped')(x1)
-
-# x3 = tf.reshape(x3, (5,5)) #output a 5 * 5 image
-
-model = Model(inputs= [ lG_inp, ext_inp], outputs= [x2,x3])
-
-
-# In[11]:
-
-
-model.summary()
-
-
-# In[12]:
-
-
-model.compile(loss=[tf.keras.losses.BinaryCrossentropy(),'categorical_crossentropy'],
-              optimizer='adam',
-              metrics=['accuracy'])
-
-
-# In[15]:
-
-
-model.fit({"lg_inp" : lg_data, "ext_inp" : lg_extract }, { "out_touch" : lg_touch, "out_cropped" : lg_croppedimg}, epochs = 5)
-
-
-# In[16]:
-
-
-model.predict({"lg_inp" : lg_data, "ext_inp" : lg_extract })
-
-
-# In[ ]:
-
-
-
-
+    return model
